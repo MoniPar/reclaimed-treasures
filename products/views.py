71b4ab910837a83f66143466aee802da3eaa77e6
MoniPar <>
2023-05-05 23:56:19 +0000

    Add shop view and template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def shop(request):
+    """
+    A view to show all products
+    """
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/shop.html', context)
