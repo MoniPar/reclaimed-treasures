931da6ca91bc02ca49afc7108c2a83663a10dd2a
MoniPar <>
2023-05-05 23:39:20 +0000

    Customise Admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name', 'name'
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku', 'name', 'category', 'theme', 'price', 'rating',
+        'stock', 'available', 'image'
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
