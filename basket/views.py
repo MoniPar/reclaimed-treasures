5ef76a6040b0a86ae1d990a1c3cac06175f082cc
MoniPar <>
2023-05-11 17:29:02 +0000

    Add add_to_basket view

--- basket/views.py
+++ basket/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def shopping_basket(request):
     """
-    A view that renders the shopping basket page
+    Renders the shopping basket page
     """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """
+    Adds quantity of the specific chosen product to the shopping basket
+    """
+    # Get the quantity and the redirect_url from the form
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    # Access the session if it exists and create it if it doesn't
+    basket = request.session.get('basket', {})
+
+    # Add item to the basket or update its quantity if already exists
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    # Override the variable in the session with the updated version
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
