7c8f2d43e3817f24c7f73957f9c6cdde8f24d53e
MoniPar <>
2023-05-17 16:52:42 +0000

    Register models, create signals & override ready method

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+
+def ready(self):
+    """
+    Lets Django know there's a new signals module with listeners
+    """
+    import checkout.signals
